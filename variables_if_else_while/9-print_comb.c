7540f6f37439721d8ec945f5154bfdc32c25968c
manar <mnarzaid247@gmail.com>
2025-10-15 09:05:42 +0000

    4th try

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
-{ 
+int main(void)
+{
 	int n = 0;
 
 	while (n <= 9)
 		{
-		putchar ('0'+ n);
+		putchar ('0' + n);
 		if (n != 9)
 			{
 			putchar (',');
