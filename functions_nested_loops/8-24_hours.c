34a5a73bc5279ccf700c653260cb6bf0097dfd92
manar <mnarzaid247@gmail.com>
2025-10-17 13:33:31 +0000

    sec try

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,12 +6,30 @@ int hour  = 0;
 while (hour < 24)
 {
 int min  = 0;
-while (min < 60)
+while (min < 7 )
 {
-_putchar(hour + '0');
+	int sec = 0;
+	if (min <= 5)
+	{
+	while (sec < 10)
+	{
+		_putchar(hour + '0');
 _putchar(':');
 	_putchar(min + '0');
+	_putchar(sec + '0');
 	_putchar('\n');
+	sec++;
+	}
+	}
+	else 
+	if ( min == 6 )
+	{
+		_putchar(hour + '0');
+		_putchar(':');
+		_putchar('0');
+		_putchar('0');
+		 _putchar('\n');
+	}
 min++;
 }
 hour++;
