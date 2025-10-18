7e88e043d696e4aa77089f19aa73db6e9174a511
manar <mnarzaid247@gmail.com.com>
2025-10-18 13:12:26 +0000

    3rd try

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,12 +9,24 @@ int num1 = 0;
 		while ( num2 <= 9)
 		{
 		int num3 = num1*num2;
+		if (num3 > 9)
+		{
 		_putchar(num3/10 + '0');
 		_putchar(num3 % 10 + '0');
 		if (num2 <= 8)
 		{
 		_putchar(',');
 		_putchar(' ');
+		}
+		}
+		else 
+		{
+		_putchar('0'+ num3);
+		if (num2 <= 8)
+                {
+                _putchar(',');
+                _putchar(' ');
+                }
 		}
 		num2++;
 		}
