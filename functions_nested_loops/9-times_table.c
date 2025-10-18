0a9ce6dbcaf0f6c5ee70ad13a6dbf0b1f55667e2
manar <mnarzaid247@gmail.com.com>
2025-10-18 13:45:22 +0000

    4th try

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,32 +1,37 @@
 #include "main.h"
 
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Always nothing (void)
+ */
 void times_table(void)
 {
 int num1 = 0;
-	while ( num1 <= 9)
+	while (num1 <= 9)
 	{
 		int num2 = 0;
-		while ( num2 <= 9)
+		while (num2 <= 9)
 		{
-		int num3 = num1*num2;
-		if (num3 > 9)
+		int num3 = num1 * num2;
+		if (num2 == 0)
 		{
-		_putchar(num3/10 + '0');
-		_putchar(num3 % 10 + '0');
-		if (num2 <= 8)
-		{
-		_putchar(',');
-		_putchar(' ');
+			_putchar(num3 + '0');
 		}
-		}
-		else 
+		else
 		{
-		_putchar('0'+ num3);
-		if (num2 <= 8)
-                {
-                _putchar(',');
-                _putchar(' ');
-                }
+		 _putchar(',');
+_putchar(' ');
+if (num3 < 10)
+{
+    _putchar(' ');
+    _putchar(num3 + '0');
+}
+		else
+		{
+		_putchar((num3 / 10) + '0');
+		_putchar((num3 % 10) + '0'); 
+		}
 		}
 		num2++;
 		}
