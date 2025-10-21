ced81482088d669b920a493dbc603a4a82daa9d5
manar <mnarzaid247@gmail.com>
2025-10-21 10:51:56 +0000

    3rd try

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,16 @@ int  main(void)
 int i = 1;
 while (i <= 100)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
+printf(" FizzBuzz ");
+	else if (i % 3 == 0)
 	printf(" Fizz ");
 else if (i % 5 == 0)
 	printf(" Buzz ");
-else if (i % 3 == 0 && i % 5 == 0)
-	printf(" FizzBuzz ");
 else
 	printf("%d", i);
 i++;
 }
-return(0);
+return (0);
 }
 
