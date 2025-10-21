b29805924c5634f22a6a9d133b0724769d9a21b3
manar <mnarzaid247@gmail.com>
2025-10-21 06:08:26 +0000

    first try

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
