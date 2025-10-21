6114f1876092ff1c79b8e424cda6e237ad568e80
manar <mnarzaid247@gmail.com>
2025-10-21 08:49:54 +0000

    first try

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+if (n <= 0)
+	_putchar('\n');
+else
+	{
+	while (n > 0)
+		{
+		_putchar('_');
+		n--;
+	}
+}
+_putchar('\n');
+}
