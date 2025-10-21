575a05dc5f7f907b1cb8bbb62a0f370457a5a416
manar <mnarzaid247@gmail.com>
2025-10-21 08:36:35 +0000

    first try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 1 ; i <= 10 ; i++)
+{
+int n;
+for (n = 1 ; n <= 14 ; n++)
+{
+if (n > 9)
+	_putchar( n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+	_putchar('\n');
+}
+}
