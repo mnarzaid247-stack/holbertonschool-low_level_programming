8e9a1916234adeb73883ad454fbef5b13ecf697f
manar <mnarzaid247@gmail.com>
2025-10-22 09:58:28 +0000

    first try

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - short description of what the function does
+ * @n: description of the parameter
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
