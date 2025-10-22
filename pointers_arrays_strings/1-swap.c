a234b5b96630df52101f6d8a6e25a4fa63230d5b
manar <mnarzaid247@gmail.com>
2025-10-22 10:07:50 +0000

    first try

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
