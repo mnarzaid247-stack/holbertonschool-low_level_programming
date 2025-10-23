7a6fdd42951491f67741bf85a1264e7542747231
manar <mnarzaud247@gmail.com>
2025-10-23 09:29:05 +0000

    sec try

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,17 @@
 void print_array(int *a, int n)
 {
 int i = 0;
+if (n <= 0)
+	printf("\n");
+else
+{
 while (i < n)
 {
 if (i != n - 1)
 printf("%d, ", a[i]);
 	else
-		printf("%d \n", a[i]);
+		printf("%d", a[i]);
 i++;
+}
 }
 }
