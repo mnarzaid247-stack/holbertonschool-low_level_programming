40dc3a206811f9cc8390e960ef3fa5b24596c74c
manar <mnarzaid247@gmail.com>
2025-10-27 09:16:44 +0000

    first try

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ * @s: character
+ * @c: character
+ *
+ * Return: character
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0 ;; i++)
+if (s[i] == c)
+{
+	return ((s+i));
+}
+	else if (s[i] == '\0')
+	return (NULL);
+}
