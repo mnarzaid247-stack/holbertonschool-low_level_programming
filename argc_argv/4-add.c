8502aa82f3447299567bff98726d3ada20e77d5c
manar <mnarzaid247@gmail.com>
2025-10-29 09:32:25 +0000

    first try

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main -  adds positive numbers
+ *
+ * @argc: integer
+ * @argv: character
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+int i = 1;
+int num;
+int j;
+int num2 = 0;
+if (argc <= 1)
+{
+printf("%d\n", num2);
+return (0);
+}
+while (i <= argc - 1)
+{
+for (j = 0 ; argv[i][j] != '\0' ; j++)
+{
+if (isdigit(argv[i][j]))
+{
+num = atoi(argv[i]);
+num2 = num2 + num;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+i++;
+}
+printf("%d\n", num2);
+return (0);
+}
