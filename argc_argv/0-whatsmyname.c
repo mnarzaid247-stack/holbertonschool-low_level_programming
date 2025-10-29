98c8a837fe5f416be0d482d03afe0be36d2c422a
manar <mnarzaid247@gmail.com>
2025-10-29 08:21:26 +0000

    first try

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ *
+ * @argv: character
+ * @argc: integer
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
