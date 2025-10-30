dbc8792a116c8232b01d551dab0a7be0ff5007a0
manar <mnarzaid247@gmail.com>
2025-10-30 12:42:10 +0000

    first try

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_strring - sets the value of a pointer to a char
+ *
+ * @s: character
+ * @to: character
+ *
+ * return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
