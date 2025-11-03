2466e566aa5882ba177a5cc146bfbbbcf9e04220
manar <mnarzaid247@gmail.com>
2025-11-03 10:08:08 +0000

    first try

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ *
+ * @size: unsigned integer
+ * @c: character
+ *
+ * Return: character
+ */
+char *create_array(unsigned int size, char c)
+{
+int i;
+if ( size == 0)
+return(NULL);
+char *p = malloc(size);
+if (p == 0)
+	return(NULL);
+for (i = 0 ; i < size ; i++)
+	p[i] = c;
+return(p);
+}
