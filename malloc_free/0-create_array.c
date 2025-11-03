e5a666f54d780bf63da19f3fbbdc0ec1a8fc998d
manar <mnarzaid247@gmail.com>
2025-11-03 10:16:18 +0000

    4th try

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,9 +13,10 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
+char *p;
 if (size == 0)
 return (NULL);
-char *p = malloc(size);
+p = malloc(size);
 if (p == 0)
 	return (NULL);
 for (i = 0 ; i < size ; i++)
