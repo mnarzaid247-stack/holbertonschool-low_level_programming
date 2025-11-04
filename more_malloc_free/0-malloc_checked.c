a5cd79089633248e0c2f9d2171bb6c951a13b5ac
manar <mnarzaid247@gmail.com.com>
2025-11-04 08:43:22 +0000

    first try

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ *
+ * @b: integer
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == NULL)
+	exit(98);
+return (p);
+}
