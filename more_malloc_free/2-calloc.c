c8f63e177296e790b574125592d6a39727d40267
manar <mnarzaid247@gmail.com.com>
2025-11-04 09:28:56 +0000

    first try

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_calloc - allocates memory for an array
+ *
+ *@nmemb: unsigned integer
+ *@size: unsigned integer
+ *
+ *Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *arr;
+unsigned int i;
+if (nmemb == 0)
+	return (NULL);
+if (size == 0)
+	return (NULL);
+arr = malloc(size * nmemb);
+if (arr == NULL)
+	return (NULL);
+for (i = 0 ; i < nmemb * size ; i++)
+	arr[i] = 0;
+return (arr);
+}
