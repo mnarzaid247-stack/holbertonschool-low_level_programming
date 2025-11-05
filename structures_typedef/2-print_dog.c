de12f00c4ba90b0985c71008a6469a1870773fc2
manar <mnarzaid247@gmail.com>
2025-11-05 10:17:07 +0000

    5th try

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,5 +17,5 @@ if (d->name == NULL)
 	d->name = "(nil)";
 if (d->owner == NULL)
 	d->owner = "(nil)";
-printf("Name: %s\n Age: %.2f\n Owner: %s ", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
