405d8b19242b2763137ecc7dc3a0af4440dc8cc8
manar <mnarzaid247@gmail.com>
2025-11-05 09:32:40 +0000

    first try

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type
+ *
+ * @name: character
+ * @age: float
+ * @owner: character
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif
