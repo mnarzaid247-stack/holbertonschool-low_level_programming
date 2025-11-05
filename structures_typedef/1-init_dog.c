ad7c86641d3be34e28f6fc17ee76f226f48a51ed
manar <mnarzaid247@gmail.com>
2025-11-05 09:50:15 +0000

    first try

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable
+ *
+ * @d: struct
+ * @name: character
+ * @age: float
+ * @owner: character
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
