038d2f6151770606e28facbc84380d73a43d20d0
manar <mnarzaid247@gmail.com>
2025-11-06 10:46:55 +0000

    first try

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,5 +17,6 @@ char *owner;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
