0534d30bb309c74c123d5cb038821438d29e8ef0
manar <mnarzaid247@gmail.com>
2025-11-10 09:54:23 +0000

    first try

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: integer
+ * @size: integer
+ * @cmp: function
+ *
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (cmp == NULL)
+	return (-1);
+if (size <= 0)
+return (-1);
+for (i = 0 ; i < size ; i++)
+{
+if (cmp(array[i]) != 0)
+return (cmp(array[i]));
+}
+else
+return (-1);
+}
