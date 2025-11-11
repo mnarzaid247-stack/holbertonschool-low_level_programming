16279f4e15352c78ef7631d5cb7d01229d91cbc3
manar <mnarzaid247@gmail.com>
2025-11-11 09:42:08 +0000

    3rd try

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 int num1;
 int num2;
+int (*op_func)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
@@ -20,7 +21,8 @@ exit(98);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-if (get_op_func(argv[2]) == NULL)
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
 {
 	printf("Error\n");
 exit(99);
