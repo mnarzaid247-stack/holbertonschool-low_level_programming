d594204a446bf408e485ecc014d136f20cc9c3dd
manar <mnarzaid247@gmail.com>
2025-11-12 08:43:05 +0000

    first try

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0;
 unsigned int i;
+va_list args;
 if (n == 0)
 	return (0);
-va_list args;
 va_start(args, n);
 for (i = 0 ; i < n ; i++)
 	sum = sum + va_arg(args, int);
-va_end (args);
+va_end(args);
 return (sum);
 }
