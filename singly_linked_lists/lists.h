bdf52c593350ac100e5c5fd0fbcf3711bec58b93
manar <mnarzaid247@gmail.com>
2025-11-26 06:37:28 +0000

    4th try:

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,7 +4,14 @@
 #include <stddef.h>
 
 int _putchar(char c);
-
+/**
+ * struct list_s - singly linked list
+ * @str: string (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 char *str;
