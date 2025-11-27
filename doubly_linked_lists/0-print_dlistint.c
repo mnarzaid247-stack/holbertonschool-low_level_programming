199891a6ecc6d44e1bb73723aab968c9a032f9db
mnar <mnarzaid247@gmail.com>
2025-11-27 09:32:53 +0000

    first try

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print the nodes
+ *
+ * @h: pointer
+ *
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t i = 0;
+const dlistint_t *tmp = h;
+while (tmp != NULL)
+{
+printf("%d\n", tmp->n);
+tmp = tmp->next;
+i++;
+}
+return (i);
+}
