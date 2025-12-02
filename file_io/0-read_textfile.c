d779c4ad41beaaa5fba444c374325cee2608ab1c
manar <mnarzaid@gmail.com>
2025-12-02 09:03:21 +0000

    first try

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - read and print a file
+ *
+ * @filename: character
+ * @letters: size_t
+ *
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int r;
+int w;
+int i;
+char *buffer = malloc(sizeof(char) * letters);
+if (filename == NULL)
+	return (0);
+if (buffer == NULL)
+	return (0);
+i = open(filename, O_RDONLY);
+if (i == -1)
+	return (0);
+r = read(i, buffer, letters);
+if (r == -1)
+{
+	free(buffer);
+	close(i);
+	return (0);
+}
+w = write(1, buffer, r);
+if (w == -1 || w != r)
+{
+free(buffer);
+close(i);
+return (0);
+}
+free(buffer);
+close(i);
+return (r);
+}
