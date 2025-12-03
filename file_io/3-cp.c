6e1a545bf22230d58214359faf47264c5f2f92c5
manar <mnarzaid247@gmail.com>
2025-12-03 09:52:23 +0000

    first try

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,4 +40,6 @@ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 if (close(o) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o), exit(100);
 if (close(oo) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", oo), exit(100); }
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", oo), exit(100);
+return (0);
+}
