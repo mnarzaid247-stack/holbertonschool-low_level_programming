bd0d9eda6a2d77c08a153bf84272c5b22d29c7a9
manar <mnarzaid247@gmail.com>
2025-12-03 05:57:57 +0000

    sec try

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 size_t l = 0;
 int o;
-int w;
+size_t w;
 if (filename == NULL)
 	return (-1);
 o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
