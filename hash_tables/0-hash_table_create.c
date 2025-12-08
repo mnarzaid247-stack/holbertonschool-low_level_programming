3620e352fe5ad1390e1b335cf4312e1e29d81457
manar <mnarzaid247@gmail.com>
2025-12-08 08:21:42 +0000

    first try

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_create - create a hash table
+ *
+ * @size: long integer
+ *
+ * Return: hash_table_t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+unsigned long int i;
+hash_table_t *new = malloc(sizeof(hash_table_t));
+if (new == NULL)
+return (NULL);
+new->size = size;
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new->array == NULL)
+{
+free(new);
+return (NULL);
+}
+for (i = 0 ; i < size ; i++)
+{
+new->array[i] = NULL;
+}
+return (new);
+}
