52a988a720055f30e49c25d43e0a6fcbb3803cc1
manar <mnarzaid247@gmail.com>
2025-12-08 08:36:38 +0000

    second try

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 unsigned long int real_key = hash_djb2(key);
 if (size == 0)
-       return (0);	
+return (0);
 real_key = real_key % size;
 return (real_key);
 }
